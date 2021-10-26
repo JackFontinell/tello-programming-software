c23346a40ecd1f34494cfa81897b598b541fe545
IainMoose <92953852+IainMoose@users.noreply.github.com>
2021-10-26 14:31:02 -0700

    Update basicmovement.py

--- basicmovement.py
+++ basicmovement.py
@@ -5,21 +5,19 @@ tello = Tello()
 tello.connect()
 tello.takeoff()
 
-tello.move_left(100)
+tello.move_forward(304.8)
 tello.rotate_counter_clockwise(90)
 
 tello.rotate.counter_clockwise(180)
-tello.move_forward(100)
+tello.move_left(304.8)
 
 tello.rotate_clockwise(90)
-tello.rotate_clockwise(30)
+
+tello.move_backward(304.8)
 tello.rotate_clockwise(90)
 
-tello.move_backward(100)
-tello.rotate_clockwise(90)
+tello.move_right(304.8)
 
-tello.move_right(10)
-
-tello.flip_forward()
+tello.flip_forward(304.8)
 
 tello.land()
