b8b907e321b8bb1691907404fb872e9d535eb4b3
IainMoose <92953852+IainMoose@users.noreply.github.com>
2021-10-28 13:40:03 -0700

    Update basicmovement.py

--- basicmovement.py
+++ basicmovement.py
@@ -1,23 +1,28 @@
 from djitellopy import Tello
-
+from time import sleep
 tello = Tello()
 
 tello.connect()
 tello.takeoff()
 
+sleep(1.5)
+tello.move_forward(304.8)
+tello.rotate_counter_counter_clcokwise(90)
+
+sleep(1.5)
 tello.move_forward(304.8)
 tello.rotate_counter_clockwise(90)
 
-tello.rotate.counter_clockwise(180)
-tello.move_left(304.8)
+sleep(1.5)
+tello.move_forward(304.8)
+tello.rotate_counter_clockwise(90)
 
-tello.rotate_clockwise(90)
+sleep(1.5)
+tello.move_forward(304.8)
 
-tello.move_backward(304.8)
-tello.rotate_clockwise(90)
-
-tello.move_right(304.8)
-
-tello.flip_forward(304.8)
+sleep(2)
+tello.flip_forward(1)
+tello.flip_backward(1)
 
 tello.land()
+
